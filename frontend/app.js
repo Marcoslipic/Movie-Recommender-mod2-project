47d801353258baaa6243846fb35f496b7c659d73
“Jawnny5” <digitalanalogbros@gmail.com>
2020-10-21 19:36:57 -0600

    basic frontend

--- frontend/app.js
+++ frontend/app.js
@@ -1,41 +1,52 @@
 const form = document.querySelector('form')
 const input = document.querySelector('#searchTerm')
-console.log(input)
-
-const apikey = "api_key=apie6c80bcc6c22dfddc5cc43795406ed56"
-const genres = "https://api.themoviedb.org/3/genre/movie/list?api_key=e6c80bcc6c22dfddc5cc43795406ed56&language=en-US"
-
-const genreURL = `https://api.themoviedb.org/3/discover/movie?${apikey}`
+const resultsSection = document.querySelector('#results')
+const apikey = "api_key=e6c80bcc6c22dfddc5cc43795406ed56"
+const genreURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=e6c80bcc6c22dfddc5cc43795406ed56&language=en-US"
+const baseURL = `https://api.themoviedb.org/3/discover/movie?${apikey}`
+const imgURL = `https://image.tmdb.org/t/p/w500`
 let genreInput = "&with_genres="
 let searchURL = ""
-
-// fetch(genres)
-// .then(response => response.json())
-// .then(data => getName(data))
-
-function getName(data){
-  data["genres"].map(entry => {
-    if entry.name == input.value
-    
-
-    console.log(data["genres"])
-  })
-}
-    
-//   // data["genres"].map(entry =>{
-//   //   // if (entry.name == input.value)
-      
-  
-
-
-
 
 form.addEventListener('submit', formSubmitted)
 
 function formSubmitted(event){
   event.preventDefault()
   const searchTerm = input.value
-  console.log(searchTerm)
+  fetch(genreURL)
+    .then(response => response.json())
+    .then(data => getName(data["genres"]))  
 }
+
+function getName(result){
+  const searchGenre = result.find(genre => {
+    return genre.name === input.value
+  })
+  genreInput = `${genreInput}${searchGenre.id}`
+  searchURL = baseURL + genreInput
+  getMovie()
+}
+
+  function renderInfo(data){
+    const randomRes = data[Math.floor (Math.random() * data.length)]
+    resultsSection.innerHTML =  `
+      <div class="card">
+        <img src="${imgURL}${randomRes["poster_path"]}" class="card-img-top" alt="${randomRes["title"]}">
+        <div class="card-body">
+          <h5 class="card-title">${randomRes["title"]}</h5>
+          <p class="card-text">${randomRes["overview"]}</p>
+          <form>
+            <input type="button" onclick="getMovie()" value="Next" />
+          </form>
+        </div>
+      </div>
+    `
+  }
+
+  function getMovie(){
+    fetch(searchURL)
+      .then(result => result.json())
+      .then(data => renderInfo(data["results"]))
+  }
 
 
